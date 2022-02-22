9e43ba0522d6548df14077dad896b3e7ef54810c
Germán Escobar <german.escobarc@gmail.com>
2022-02-22 12:09:11 -0500

    Remove Links

--- src/pages/index.js
+++ src/pages/index.js
@@ -154,24 +154,7 @@ const IndexPage = () => {
             {docLink.text}
           </a>
         </li>
-        {links.map(link => (
-          <li key={link.url} style={{ ...listItemStyles, color: link.color }}>
-            <span>
-              <a
-                style={linkStyle}
-                href={`${link.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter`}
-              >
-                {link.text}
-              </a>
-              {link.badge && (
-                <span style={badgeStyle} aria-label="New Badge">
-                  NEW!
-                </span>
-              )}
-              <p style={descriptionStyle}>{link.description}</p>
-            </span>
-          </li>
-        ))}
+
       </ul>
       <img
         alt="Gatsby G Logo"
